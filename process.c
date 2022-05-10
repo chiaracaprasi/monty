5a8850bc71074c9b7a723fac52548f4d055fb71c
Karoline-S <karoline.amy@gmai.com>
2022-05-10 06:29:29 -0700

    build - c file for tokenising and building the stack

--- process.c
+++ process.c
@@ -2,47 +2,60 @@
 
 /**
  * process - breaks up the buffer into tokens and sends for processing
- * @lineBuffer: the buffer containing the line from the bytecode
+ * @lineBuffer: a buffer containing the line from bytecode file
  * Return: 0 for success, -1 for any error
  */
-int process(char *lineBuffer)
+int tokenise(char *lineBuffer)
 {
 	char *token;
-	char *hold_opCode = NULL;
 
 	token = strtok(lineBuffer, " ");
-	printf("the first token is %s\n", token);
+
+	if (strcmp(token, "#") == 0)
+	{
+		printf("found a comment\n");
+		return (0);
+	}
+
+	if (strcmp(token, "stack") == 0)
+		access.mode = 0;
+	printf("mode int is %d\n");
+
+	if (strcmp(token, "queue") == 0)
+		access.mode = 1;
+	printf("mode int is %d\n");
 
 	if (strcmp(token, "push") == 0)
 	{
-		hold_opCode = token;
 		token = strtok(NULL, " ");
-		access.pushArg = atoi(token);
-		if (access.pushArg == 0 && !strcmp(token, "0"))
+
+		if (token == NULL ||( atoi(token) == 0 && strcmp(token, "0") != 0))
 		{
-			printf("printing error here");
+			printf("print error re push arg\n");
 			return (-1);
 		}
+		if (stack_builder(atoi(token)) == -1)
+		{
+			printf("printing malloc error here");
+			return (-1);
+		}
+		return (0);
 	}
-
-	if (hold_opCode != NULL)
-		get_func(hold_opCode);
-	else
-		get_func(token);
-
+	printf("calling get function here\n");
+/*	return (get_func(token));*/
 	return (0);
 }
-
+/*
 int get_func(char *opCode)
 {
 	int idx = 0;
 	instruction_t selector[] = {
-		{"push", push_op},
 		{"pall", pall_op},
-		{"pint", pint_op}
+		{"pint", pint_op},
+		{NULL, NULL}
 	};
 
-	while (selector[idx] != NULL)
+	while (selector[idx].opcode != NULL)
 	{
 		if (opCode == selector[idx].opcode)
 		{
@@ -51,6 +64,91 @@ int get_func(char *opCode)
 		}
 		idx++;
 	}
-	printf("error here");
+	printf("printing error for bad opcode");
+	return (-1);
+}
+*/
+int stack_builder(int n)
+{
+	stack_t *new;
+
+	if (access.mode == 0)
+		new = push_node();
+	else
+		new = enqueue_node();
+
+	if (new == NULL)
+		return (-1);
+
+	new->n = n;
+
 	return (0);
+}
+
+stack_t *push_node()
+{
+	stack_t **head = access.head;
+	stack_t *new;
+
+	new = malloc(sizeof(*new));
+
+	if (new == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+
+	if ((*head)->next == NULL)
+	{
+		new->prev = *head;
+		new->next = *head;
+		(*head)->prev = new;
+		(*head) = new;
+		return (new);
+	}
+
+	new->prev = (*head)->prev;
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
+	return (new);
+}
+
+stack_t *enqueue_node()
+{
+	stack_t **head = access.head;
+	stack_t *new;
+
+	new = malloc(sizeof(*new));
+
+	if (new == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+
+	if ((*head)->next == NULL)
+	{
+		new->prev = *head;
+		new->next = NULL;
+		(*head)->prev = new;
+		(*head)->next = new;
+		return (new);
+	}
+
+	new->prev = (*head)->prev;
+	new->next = NULL;
+	new->prev->next = new;
+	(*head)->prev = new;
+	return (new);
 }
