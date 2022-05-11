864bda532e156be788cc0cf26c063fcd246ebdd8
Karoline-S <karoline.amy@gmai.com>
2022-05-11 14:54:40 -0700

    build - add sub_op code

--- add_op.c
+++ add_op.c
@@ -17,3 +17,22 @@ void add_op(stack_t **stack, unsigned int line_number)
 	pop_op(stack, line_number);
 	(*stack)->n = n;
 }
+
+/**
+ * sub_op - subs the first int from the next, held in a stack_t
+ * doubly linked list
+ * @stack: pointer to the head of the list
+ * @line_number: line no\umber opcode was found in
+ * Return: Nothing
+ */
+void sub_op(stack_t **stack, unsigned int line_number)
+{
+	int n;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		error_handler(7, "sub");
+
+	n = (*stack)->next->n - (*stack)->n;
+	pop_op(stack, line_number);
+	(*stack)->n = n;
+}
